4adb0329997edbdfdec7b249d768778db008a7c7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-11 10:29:00 -0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Mitch Andrade on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
